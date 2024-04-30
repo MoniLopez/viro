4e669bd4a039d73da03860d42ea1bff04ec3f3af
MoniLopez <133059943+MoniLopez@users.noreply.github.com>
2024-04-30 17:38:38 -0600

    HTML y CSS de componente login close #1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', redirectTo: '/login', pathMatch: 'full' }, //redirije al componente login
+    { path: 'login', component: LoginComponent } //Indica quién es login
+];
